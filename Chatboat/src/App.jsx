ecf68285b917a4cf0887a475e934f7056b11b1ed
Amit72777 <amitkushw2005@gmail.com>
2026-03-18 17:52:36 +0530

    removed api key

---
+++ Chatboat/src/App.jsx
@@ -0,0 +1,17 @@
+
+import "./App.css";
+import  GeminiChatboat from "./Chatbot/GeminiChatbot.jsx";
+import GroqChatbox from "./Chatbot/GroqChatbot.jsx";
+
+function App() {
+
+  return (
+
+    <>
+    {/* <GeminiChatboat/> */}
+    <GroqChatbox/>
+    </>
+  );
+}
+
+export default App;
