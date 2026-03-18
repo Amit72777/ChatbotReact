cb0df0d31172f49f8d2120b24fb30a64fb0cf4ed
Amit72777 <amitkushw2005@gmail.com>
2026-03-18 17:58:05 +0530

    clean project (removed api key)

--- Chatboat/src/App.jsx
+++ Chatboat/src/App.jsx
@@ -1,15 +1,15 @@
 
 import "./App.css";
 import  GeminiChatboat from "./Chatbot/GeminiChatbot.jsx";
-import GroqChatbox from "./Chatbot/GroqChatbot.jsx";
+
 
 function App() {
 
   return (
 
     <>
-    {/* <GeminiChatboat/> */}
-    <GroqChatbox/>
+    <GeminiChatboat/>
+    {/* <GroqChatbox/> */}
     </>
   );
 }
